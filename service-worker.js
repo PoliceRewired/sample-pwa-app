c30fc7e8599d409d5918c6b23f8ae387965bc8b1
Lewis Westbury <lewis@Lewiss-MBP.connect>
2019-02-04 16:15:03 +0000

    update sw

--- service-worker.js
+++ service-worker.js
@@ -5,11 +5,13 @@ self.addEventListener('install', function(event) {
         .open('sample-pwa-cache')
         .then(function(cache) {
           return cache.addAll([
+            /*
             '/sample-pwa-app/',
             '/sample-pwa-app/index.html',
             '/sample-pwa-app/images/*',
             '/sample-pwa-app/css/*',
             '/sample-pwa-app/scripts/*'
+            */
           ]);
         })
         .then(function() {
